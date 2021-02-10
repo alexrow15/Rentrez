f331faeb5dec89f21aed80981503d53136d22a73
Alexander Row <17ajr@queensu.ca>
2021-02-10 10:41:04 -0500

    split into each of the 3 different sequences

---
+++ Download.R
@@ -0,0 +1,11 @@
+#Assignment 5 
+
+install.packages("rentrez")
+
+ncbi_ids <- c("HQ433692.1","HQ433694.1","HQ433691.1") #creating a character class of ncbi Id's
+library(rentrez)  #load rentrez package 
+Bburg<-entrez_fetch(db = "nuccore", id = ncbi_ids, rettype = "fasta") #entrez_fetch gets a complete reresentation (DNA sequence) of the record of interest. In this case,
+#the rettype is fasta, so a fasta type will be found and summarized 
+
+Sequences <- strsplit(Bburg, split = "\n\n")
+Sequences
