8df09a905447a675105595d12fd598d9fdccca2f
Alexander Row <17ajr@queensu.ca>
2021-02-12 13:22:56 -0500

    add comments

--- Download.R
+++ Download.R
@@ -7,13 +7,15 @@ library(rentrez)  #load rentrez package
 Bburg<-entrez_fetch(db = "nuccore", id = ncbi_ids, rettype = "fasta") #entrez_fetch gets a complete reresentation (DNA sequence) of the record of interest. In this case,
 #the rettype is fasta, so a fasta type will be found and summarized 
 
-Sequences <- strsplit(Bburg, split = "\n\n")
+Sequences <- strsplit(Bburg, split = "\n\n")  #separate the 3 variants by \n\n
 
-Sequences<-unlist(Sequences)
+Sequences<-unlist(Sequences) 
 
-header<-gsub("(^>.*sequence)\\n[ATCG].*","\\1",Sequences)
-seq<-gsub("^>.*sequence\\n([ATCG].*)","\\1",Sequences)
-Sequences<-data.frame(Name=header,Sequence=seq)
-Sequences
+header<-gsub("(^>.*sequence)\\n[ATCG].*","\\1",Sequences)  #select only the header
+seq<-gsub("^>.*sequence\\n([ATCG].*)","\\1",Sequences)    #select only the sequence
+Sequences<-data.frame(Name=header,Sequence=seq)  #create dataframe
 
-Sequences <- gsub("\n", "", Sequences$Sequence)
+Sequences <- gsub("\n", "", Sequences$Sequence)  #remove newline characters
+Sequences <- data.frame(Name = header, Sequence = Sequences)   #create dataframe of sequence and variant
+write.csv(Sequences, "Sequences.csv")   #write out the data to file called Seqeunces.csv
+
